1eb429a5e9f824cf2ecf8dad1bb395cbe402b5dd
Pedro Nunes <pedronunesjc@gmail.com>
2022-05-29 23:06:46 -0300

    Criado RepositorioQuestaoEmBancoDeDados

---
+++ GeradorDeTestes.Infra.BancoDeDados/ModuloQuestão/RepositorioQuestaoEmBancoDeDados.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation.Results;
+using GeradorTestes.Dominio.ModuloQuestao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorDeTestes.Infra.BancoDeDados.ModuloQuestão
+{
+    internal class RepositorioQuestaoEmBancoDeDados : IRepositorioQuestao
+    {
+
+
+        public ValidationResult Editar(Questao registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValidationResult Excluir(Questao registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValidationResult Inserir(Questao novoRegistro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Questao SelecionarPorNumero(int numero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Questao> SelecionarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
